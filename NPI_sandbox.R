9b18e2671dc7c8439e33480272e9ac510322c02d
Emmett Leyne <emmettleyne@gmail.com>
2019-03-25 16:19:14 +1100

    Added Field site Dale

--- NPI_sandbox.R
+++ NPI_sandbox.R
@@ -1,5 +1,7 @@
-library(tidyverse)
+## Add tidyverse
+(tidyverse)
 
+# Create the subset of lines and TOS that are to have extra data taken
 sel_subset <- "Axe, Beaufort, Beckom, Cutlass, Gregory, Kittyhawk, Lancer, Mace, Manning, Scepter,
 Trojan, Suntop, Commander, Compass, Fathom, Planet, Spartacus, Urambie, EGA_Gregory, RGT Planet, Spartacus CL"
 sel_subset <- str_split(sel_subset,",")
@@ -10,35 +12,42 @@ sel_subset
 tos_subset <- c('1','4','8')
 
 
-
-wagga <- read_csv("WaggaWagga2019_Design.csv") 
+ #Wagga
+wagga <- read_csv("raw_data/WaggaWagga2019_Design.csv") 
 wagga <- wagga %>%
   select(-(X1)) %>% 
   mutate(subset=if_else((wagga$Variety %in% sel_subset & wagga$TOS %in% tos_subset), "subset\\u0007", ""))
-write_csv(wagga, file.path("2019_npi_waggawagga.csv"))
+write_csv(wagga, file.path("fp_files/2019_npi_waggawagga.csv"))
 
-callington <- read_csv("Callington2019_Design.csv")
+#Callington
+callington <- read_csv("raw_data/Callington2019_Design.csv")
 callington <- callington %>% 
   select(-(X1)) %>% 
   mutate(subset=if_else(callington$Variety %in% sel_subset & callington$TOS %in% tos_subset, "subset\\u0007", ""))
-write_csv(callington, file.path("2019_npi_callington.csv"))
+write_csv(callington, file.path("fp_files/2019_npi_callington.csv"))
 
-merridin <- read_csv("Merridin_New_Design.csv")
+#Merridin
+merridin <- read_csv("raw_data/Merridin_New_Design.csv")
 merridin <- merridin %>% 
   mutate(subset=if_else(merridin$Variety %in% sel_subset & merridin$TOS %in% tos_subset, "subset\\u0007", ""))
-write_csv(merridin, file.path("2019_npi_merridin.csv"))
+write_csv(merridin, file.path("fp_files/2019_npi_merridin.csv"))
 
-yanyean <- read_csv("Yan Yean2019_Design.csv")
+#Yan Yean
+yanyean <- read_csv("raw_data/Yan Yean2019_Design.csv")
 yanyean <- yanyean %>% 
   select(-(X1)) %>% 
   mutate(subset=if_else(yanyean$Variety %in% sel_subset & yanyean$TOS %in% tos_subset, "subset\\u0007", ""))
-write_csv(yanyean, file.path("2019_npi_yanyean.csv"))
+write_csv(yanyean, file.path("fp_files/2019_npi_yanyean.csv"))
 
-
-NPIsandbox <- read_csv("WaggaWagga2019_Design.csv")
+#Sandbox
+NPIsandbox <- read_csv("raw_data/WaggaWagga2019_Design.csv")
 NPIsandbox <- NPIsandbox %>% 
   select(-(X1)) %>% 
   mutate(subset=if_else(NPIsandbox$Variety %in% sel_subset & NPIsandbox$TOS %in% tos_subset, "subset\\u0007", ""))
-write_csv(NPIsandbox, file.path("NPIsandbox.csv"))
+write_csv(NPIsandbox, file.path("fp_files/NPIsandbox.csv"))
 
-
+#Dale
+dale <- read_csv("raw_data/Dale_2019.csv")
+dale <- dale %>% 
+  mutate(subset=if_else(dale$Variety %in% sel_subset & dale$TOS %in% tos_subset, "subset\\u0007", ""))
+write_csv(dale, file.path("fp_files/2019_npi_dale.csv"))
